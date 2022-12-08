d4a5c292a21801e86c242138380167f91c1686ca
Giordano Temple <giordanotemple@gmail.com>
2022-12-08 17:06:51 +0000

    Use a while loop to repeat a chorus line

--- conditionals.js
+++ conditionals.js
@@ -30,3 +30,7 @@ if (isCitizen && age > 18) {
 if (temperature <-40 || temperture > 40) {
   console.log("Maybe going outside isn't such a great idea...");
 };
+
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+};
