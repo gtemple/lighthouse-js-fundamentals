3fa497c250f47c7c3e7ce879b082bccb681e8cbf
Giordano Temple <giordanotemple@gmail.com>
2022-12-08 19:39:12 +0000

    perfect shot assignemnt completed

---
+++ x-marks-the-perfect-shot.js
@@ -0,0 +1,20 @@
+const moves = ['north', 'north', 'west', 'west', 'north', 'east','north'];
+
+const finalPosition = function (moves) {
+  x = 0;
+  y = 0;
+  for (let i in moves) {
+    if (moves[i] === 'north') {
+      y += 1;
+    } else if (moves[i] === 'south') {
+      y -= 1;
+    } else if (moves[i] === 'east') {
+      x += 1;
+    } else if (moves[i] === 'west') {
+      x -= 1;
+    };
+  };
+  return [x, y];
+}
+
+console.log(finalPosition(moves));
