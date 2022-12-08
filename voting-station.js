9095149a99fdcb483153b3f601e4f1202e727c1d
Giordano Temple <giordanotemple@gmail.com>
2022-12-08 19:24:04 +0000

    completed voting station assignment

---
+++ voting-station.js
@@ -0,0 +1,25 @@
+const station = [
+  ['A', 10, 'school'],
+  ['B', 9, 'restaurant'],
+  ['C', 21, 'community centre'],
+  ['D', 15, 'school'],
+  ['E', 50, 'restaurant'],
+  ['F', 20, 'school'],
+  ['G', 40, 'community centre'],
+  ['H', 50, 'school'],
+]
+
+
+function chooseStations(stations) {
+  let goodStations = [];
+  for (let i = 0; i < stations.length; i++) {
+    if (stations[i][1] >= 20 && (stations[i][2] === "school" || stations[i][2] === "community centre")) {
+      goodStations.push(stations[i][0]);
+    }
+  }
+  //console.log(goodStations);
+  return goodStations;
+}
+
+console.log(chooseStations(station));
+
