00c72dd2596936337f4b72cb136cf77d6872ac2c
Giordano Temple <giordanotemple@gmail.com>
2022-12-09 16:57:26 +0000

    looping ranges assignment complete

---
+++ looping-rage.js
@@ -0,0 +1,17 @@
+function range(start, end, step) {
+  let num = start;
+  let arr = [];
+  if (step > 0 && start < end) {
+    while (num <= end) {
+      arr.push(num);
+      num += step;
+    }
+  } 
+  return arr;
+}
+console.log(range(0, 10, 2));
+console.log(range(10, 30, 5));
+console.log(range(-5, 2, 3));
+console.log(range(0, 10, -2));
+console.log(range(10, 30, undefined));
+console.log(range(-5, 2, 0));
