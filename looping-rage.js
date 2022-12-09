a066e8306f7f14c9a84a3c38a3ade99175b7c2b5
Giordano Temple <giordanotemple@gmail.com>
2022-12-09 20:24:09 +0000

    concatenate array assignment

--- looping-rage.js
+++ looping-rage.js
@@ -9,9 +9,9 @@ function range(start, end, step) {
   } 
   return arr;
 }
-console.log(range(0, 10, 2));
-console.log(range(10, 30, 5));
-console.log(range(-5, 2, 3));
-console.log(range(0, 10, -2));
-console.log(range(10, 30, undefined));
-console.log(range(-5, 2, 0));
+// console.log(range(0, 10, 2));
+// console.log(range(10, 30, 5));
+// console.log(range(-5, 2, 3));
+// console.log(range(0, 10, -2));
+// console.log(range(10, 30, undefined));
+// console.log(range(-5, 2, 0));
