2b623e6b4e1f17bbb8a3a83f6cb82b96808ddc5e
Giordano Temple <giordanotemple@gmail.com>
2022-12-20 08:34:09 +0000

    queen thread set up

---
+++ katas/queenThreatDetector.js
@@ -0,0 +1,15 @@
+let whiteQueen = [0, 0];
+let blackQueen = [5, 7];
+
+function generateBoard(white, black) {
+  let board = []
+
+  let i = 0;
+  while (i < 8) {
+    
+  }
+}
+
+let generatedBoard = generateBoard(whiteQueen, blackQueen);
+console.log(generatedBoard);
+console.log(queenThreat(generatedBoard));
